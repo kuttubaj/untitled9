c41477a7692973e7875fe5734ac743ec07a11814
kuttubaj <iskakovkuttuubaj0207@gmail.com>
2023-09-19 19:00:03 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,109 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+import java.util.Random;
+
+public class Main {
+    public static int bossHeals = 700;
+    public static int bossDamage = 50;
+    public static int[] heroHeals = {250, 230, 210, 260};
+    public static int[] heroDamage = {25, 25, 20, 0};
+    public static String[] heroAttackType = {"Warrior", "MagicK", "Telepat", "Medic"};
+    public static String bossBarrier;
+    public static int round = 0;
+
+    public static void main(String[] args) {
+        printStatistics();
+        while (!isGameFinished()) {
+            round++;
+            round();
+
+        }
+    }
+
+    public static void bossTypeBarrier() {
+        Random random = new Random();
+        int index = random.nextInt(heroAttackType.length);
+        bossBarrier = heroAttackType[index];
+        System.out.println("Boss barrier type for " + bossBarrier);
+        bossHeals = bossHeals + heroDamage[index];
+    }
+
+    public static boolean isGameFinished() {
+        boolean allHeroesDead = true;
+        if (bossHeals <= 0) {
+            System.out.println("HEROES WIN!!!");
+            return true;
+        }
+        for (int i = 0; i < heroHeals.length; i++) {
+            if (heroHeals[i] > 0) {
+                allHeroesDead = false;
+                break;
+            }
+        }
+        if (allHeroesDead) {
+            System.out.println("BOSS WIN!!!");
+        }
+        return allHeroesDead;
+    }
+
+    public static void printStatistics() {
+        System.out.println("STATISTIC");
+        System.out.println("Boss heals " + bossHeals + ";" + " damage " + bossDamage);
+        for (int i = 0; i < heroAttackType.length; i++) {
+            System.out.println("Hero heals " + heroAttackType[i]
+                    + " " + heroHeals[i] + " damage " + heroDamage[i]);
+        }
+    }
+
+    public static void bossHits() {
+        for (int i = 0; i < heroAttackType.length; i++) {
+            if (heroHeals[i] < bossDamage) {
+                heroHeals[i] = 0;
+            } else {
+                heroHeals[i] = heroHeals[i] - bossDamage;
+            }
+        }
+    }
+
+    public static void heroHits() {
+        for (int i = 0; i < heroAttackType.length; i++) {
+            if (bossHeals < heroDamage[i]) {
+                bossHeals = 0;
+            } else {
+                bossHeals = bossHeals - heroDamage[i];
+            }
+        }
+    }
+
+
+    public static void round() {
+        System.out.println("ROUND - " + round);
+        bossHits();
+        heroHits();
+        medic();
+        bossTypeBarrier();
+        printStatistics();
+    }
+
+    public static void medic() {
+
+        boolean diadMedic;
+        diadMedic = false;
+
+        for (int i = 0; i < heroHeals.length; i++) {
+
+            if (heroAttackType[i] == "Medic" && heroHeals[i] > 0){
+                diadMedic=true;
+            }
+        }
+        for (int i = 0; i < heroHeals.length; i++) {
+            if (heroHeals[i] <= 100 && heroHeals[i] > 0 && diadMedic) {
+                heroHeals[i] = heroHeals[i] + 75;
+                System.out.println(" medic heals up " + heroAttackType[i]);
+                break;
+            }
+
+        }
+    }
+}
